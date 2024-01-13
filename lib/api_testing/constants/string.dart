ecc5749c8b80bca74be85cb5b045a57ec5a39101
Atuoha Anthony <atuohainitiatives@gmail.com>
2024-01-13 09:37:17 +0100

    update

---
+++ lib/api_testing/constants/string.dart
@@ -0,0 +1,4 @@
+class String {
+  static const imgUrl =
+      'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg';
+}
---
+++ lib/api_testing/screens/users_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UsersScreen extends StatefulWidget {
+  const UsersScreen({super.key});
+
+  @override
+  State<UsersScreen> createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
